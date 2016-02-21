3604432bb468a836a3f632c52ca3514d4c37c305
XH <276602624@qq.com>
2016-02-21 15:56:35 +0800

    new project

--- Service04_aidl_server/src/com/example/aidl/server/CalculateServer.java
+++ Service04_aidl_server/src/com/example/aidl/server/CalculateServer.java
@@ -1,5 +1,7 @@
 package com.example.aidl.server;
 
+import java.util.HashMap;
+
 import com.example.aidl.CalculateInterface;
 
 import android.app.Service;
@@ -9,7 +11,7 @@ import android.os.RemoteException;
 import android.util.Log;
 
 public class CalculateServer extends Service{
-
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		Log.i("info", "-- onBind --");
